4a3f029c11d8c6aa598e2a539acefc8f451b4edd
19Edwin92 <b_a_s_e@naver.com>
2023-04-12 11:16:28 +0900

    [styled] 레이아웃

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layoutmodel1 from '../layout/Layoutmodel1'
-import Layoutmodel2 from '../layout/Layoutmodel2'
-import Layoutmodel3 from '../layout/Layoutmodel3'
-import Layoutmodel4 from '../layout/Layoutmodel4'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layoutmodel1 from "../layout/Layoutmodel1";
+import Layoutmodel2 from "../layout/Layoutmodel2";
+import Layoutmodel3 from "../layout/Layoutmodel3";
+import Layoutmodel4 from "../layout/Layoutmodel4";
+import { ContainerWrap, MainWrap, Wrap } from "../components/GlobalStyled";
 
 function Router() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='' element={<Layoutmodel1/>}/>
-      <Route path='2' element={<Layoutmodel2/>}/>
-      <Route path='3' element={<Layoutmodel3/>}/>
-      <Route path='4' element={<Layoutmodel4/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Wrap>
+          <MainWrap>
+            <ContainerWrap>
+              <Routes>
+                <Route path="" element={<Layoutmodel1 />} />
+                <Route path="2" element={<Layoutmodel2 />} />
+                <Route path="3" element={<Layoutmodel3 />} />
+                <Route path="4" element={<Layoutmodel4 />} />
+              </Routes>
+            </ContainerWrap>
+          </MainWrap>
+        </Wrap>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default Router
+export default Router;
