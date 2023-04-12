bc858bad985dd807451f9f6690c22bdcefa9853f
19Edwin92 <b_a_s_e@naver.com>
2023-04-12 11:26:34 +0900

    innerDiv

--- src/layout/Layoutmodel1.jsx
+++ src/layout/Layoutmodel1.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 import Header from '../components/Header'
 import { Article } from '../components/GlobalStyled'
 import styled from 'styled-components'
+import { Flex } from '../components/Flex'
 
 function Layoutmodel1() {
   return (
     <>
     <Header/>
     <Article>
-      <Layout>콘텐츠 영역</Layout>
+      <MainFlex ai="center" gap="19" fw="wrap">
+        {[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map(el => <Artgrambox></Artgrambox>)}
+      </MainFlex>
     </Article>
     </>
   )
@@ -21,4 +24,22 @@ const Layout = styled.div`
   width: 1675px;
   min-height: 100px;
   background-color: yellow;
+  text-align: right;
+`
+
+const MainFlex = styled(Flex)`
+  width:auto;
+  min-height:400px;
+  margin: 50px 75px 100px;
+  /* background-color:yellow; */
+`
+
+const Artgrambox = styled(Flex)`
+  width:364px;
+  min-width:364px;
+  min-height:436px;
+  max-height:436px;
+  background-color:red;
+  border-radius: 8px;
+  position: relative;
 `
