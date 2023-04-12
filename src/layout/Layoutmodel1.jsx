9327525e2772bd04bb3aed887ddf46275255bdd2
19Edwin92 <b_a_s_e@naver.com>
2023-04-12 11:18:36 +0900

    [styled] 레이아웃

--- src/layout/Layoutmodel1.jsx
+++ src/layout/Layoutmodel1.jsx
@@ -2,16 +2,23 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Article } from '../components/GlobalStyled'
+import styled from 'styled-components'
 
 function Layoutmodel1() {
   return (
     <>
     <Header/>
     <Article>
-      <div>콘텐츠 영역</div>
+      <Layout>콘텐츠 영역</Layout>
     </Article>
     </>
   )
 }
 
 export default Layoutmodel1
+
+const Layout = styled.div`
+  width: 1675px;
+  min-height: 100px;
+  background-color: yellow;
+`
